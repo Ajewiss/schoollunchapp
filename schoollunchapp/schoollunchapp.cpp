745f6ca7d99726d8f74c77110a35eada44152001
Anton Jewiss <anton.jewiss@zoho.com>
2022-07-26 19:47:13 +1200

    created login, started menus

--- schoollunchapp/schoollunchapp.cpp
+++ schoollunchapp/schoollunchapp.cpp
@@ -1,20 +1,119 @@
-// schoollunchapp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//Program execution beginsand ends there.
+ 
 
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int getChoice() {
+	cout << endl << "LOGIN TO SCHOOL LUNCH SYSTEM" << endl;
+	cout << "(1) Create account" << endl;
+	cout << "(2) Login" << endl;
+	cout << "(3) Quit application" << endl;
+	int choice;
+	cin >> choice;
+	return choice;
 }
+
+string getEmail() {
+	string email;
+	cout << "Email: ";
+	cin >> email;
+	return email;
+}
+
+string getPassword() {
+	string password;
+	cout << "Password: ";
+	cin >> password;
+	return password;
+}
+
+bool auth(string email, string password) {
+	string line;
+	bool foundEmail = false;
+	fstream fs;
+	fs.open("data.txt", fstream::in);
+	while (getline(fs, line)) {
+		if (line == email) {
+			foundEmail = true;
+		}
+		if (line == password && foundEmail) {
+			fs.close();
+			return true;
+		}
+	}
+	return false;
+}
+
+void createAccount(string email, string password) {
+	fstream fs;
+	fs.open("data.txt", fstream::app);
+	fs << endl << email << endl << password;
+	fs.close();
+}
+
+int main() {
+	bool running = true;
+	bool login = false;
+
+	string email, password;
+	int choice;
+	while (running) {
+		choice = getChoice();
+		system("cls");
+		switch (choice) {
+		case 1:
+			email = getEmail();
+			password = getPassword();
+			createAccount(email, password);
+			cout << endl << "ACCOUNT CREATED" << endl;
+			break;
+
+		case 2:
+			email = getEmail();
+			password = getPassword();
+			if (auth(email, password) == true) {
+				cout << endl << "LOGGED IN" << endl;
+				login = true;
+				break;
+			}
+			else {
+				cout << endl << "INVALID CREDENTIALS" << endl;
+				break;
+			}
+			break;
+
+		case 3:
+			running = false;
+			break;
+		}
+	}
+	if (login == true)
+	{
+		system("cls");
+		cout << "\n\nSchool Lunch System\n\n";
+		cout << " students name : \n";
+		cout << " class room : \n";
+		cout << "view the menu : \n";
+		cout << "show orders";
+
+	}
+
+	return 0;
+}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Tips for Getting Started:
+// 1. Use the Solution Explorer window to add/manage files
+// 2. Use the Team Explorer window to connect to source control
+// 3. Use the Output window to see build output and other messages
+// 4. Use the Error List window to view errors
+// 5. Go to Project > Add New Item to create new code files, or
+//Project > Add Existing Item to add existing code files to the project
+// 6. In the future, to open this project again, go to File > Open >
+//Projectand select the.sln file
